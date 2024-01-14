1551b9b66bc66bff40da01af575c5f9b84a1b85f
ScuzaaV1 <omarbm2090@gmail.com>
2024-01-14 12:38:28 +0100

    adding files

--- omar.js
+++ omar.js
@@ -2,7 +2,7 @@ function myFunctionone() {
     let person = prompt("Please enter your name", "Mohamed Amin Boujelbena");
     if (person != null) {
       document.getElementById("hello").innerHTML =
-      "Hello " + person + "! How are you today?  I Hope You Are Fine";
+      "Hello " + person + "! How are you today?";
     }
   }
 
@@ -20,6 +20,15 @@ function myFunctionone() {
         break;
       case "gaming":
         text = "Really? Are You Sure The Gaming Is Your Favorite?";
+        break;
+        case "football":
+        text = "Oh Nice";
+        break;
+        case "gym":
+        text = "Gym Is My Favourite Too  For Your Health";
+        break;
+        case "study":
+        text = "You Will Be Better";
         break;
       default:
         text = "I have never heard of that one..";
